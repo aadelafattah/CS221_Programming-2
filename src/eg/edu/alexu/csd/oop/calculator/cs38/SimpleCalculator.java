d10e6ab64d4cdd6df9e569365a2903fd865f6003
AbdelRahman AbdelFattah <a.adel.a.fattah@gmail.com>
2019-10-14 22:34:11 +0200

    Basic input and basic getResult

--- src/eg/edu/alexu/csd/oop/calculator/cs38/SimpleCalculator.java
+++ src/eg/edu/alexu/csd/oop/calculator/cs38/SimpleCalculator.java
@@ -3,22 +3,25 @@ import java.util.regex.*;
 import java.util.*;
 
 public class SimpleCalculator implements Calculator {
-
+	
+	String memory [][] = new String [6][4];
+	
 	@Override
 	public void input(String s) {
 		
-		Scanner sc = new Scanner(System.in);
-		s=sc.nextLine();
 		String number1 ="\\d+\\.\\d+|\\d+";
 		String symbols = "[+/*-]";
 		Pattern num1 = Pattern.compile(number1);
 		Pattern sym = Pattern.compile(symbols);
 		Matcher m1 = num1.matcher(s);
 		Matcher mS = sym.matcher(s);
+		int i = 0;
 		while (m1.find()) {
-			System.out.print(m1.group());
+			memory[0][i]= m1.group();
+			i++;
 			if(mS.find()) {
-				System.out.print(mS.group());
+				memory[0][i] = (mS.group());
+				i++;
 			}	
 		}
 				
@@ -26,8 +29,32 @@ public class SimpleCalculator implements Calculator {
 
 	@Override
 	public String getResult() {
-		// TODO Auto-generated method stub
-		return null;
+		double n1=Double.valueOf(memory[0][0]);
+		double n2=Double.valueOf(memory[0][2]);
+		if(memory[0][1].equals("+")) {
+			memory[0][3]=String.valueOf(n1+n2);
+		}
+		else if(memory[0][1].equals("-")) {
+			memory[0][3]=String.valueOf(n1-n2);
+		}
+		else if(memory[0][1].equals("*")) {
+			memory[0][3]=String.valueOf(n1*n2);
+		}
+		else if(memory[0][1].equals("/")) {
+			if(Double.valueOf(memory[0][2])==0) {
+				memory[0][3]="MATH ERROR";
+			}
+			else{
+				memory[0][3]=String.valueOf(n1/n2);
+			}
+		}
+		if(Double.valueOf(memory[0][3])%1 == 0 && Double.valueOf(memory[0][2]) != 0.0) {
+			Matcher m = Pattern.compile("\\d+").matcher(memory[0][3]);
+			m.find();
+			memory[0][3]=m.group();
+		}
+		String r=memory[0][0] + memory[0][1] + memory[0][2] + "=" + memory[0][3];
+		return r;
 	}
 
 	@Override
@@ -64,8 +91,13 @@ public class SimpleCalculator implements Calculator {
 		while(true){
 			
 			String IP= "";
+			Scanner sc = new Scanner(System.in);
+			IP=sc.nextLine();
 			SimpleCalculator calc = new SimpleCalculator();
 			calc.input(IP);
+			String result = calc.getResult();
+			System.out.println(result);
+			System.out.println("");
 			
 		}
 	}
