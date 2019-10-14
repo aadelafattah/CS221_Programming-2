59d8903249060766ca0d14b06537cf5be1582b8f
AbdelRahman AbdelFattah <a.adel.a.fattah@gmail.com>
2019-10-14 18:39:29 +0200

    Given only

---
+++ src/eg/edu/alexu/csd/oop/calculator/cs38/SimpleCalculator.java
@@ -0,0 +1,53 @@
+package eg.edu.alexu.csd.oop.calculator.cs38;
+import java.util.regex.*;
+
+public class SimpleCalculator implements Calculator {
+
+	@Override
+	public void input(String s) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getResult() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String current() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String prev() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void load() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
