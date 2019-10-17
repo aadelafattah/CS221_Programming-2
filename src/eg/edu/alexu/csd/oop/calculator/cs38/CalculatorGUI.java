465696ea96d35b63198c44f88e6abfffa1729a91
AbdelRahman AbdelFattah <a.adel.a.fattah@gmail.com>
2019-10-17 06:37:31 +0200

    Final v1.0

--- src/eg/edu/alexu/csd/oop/calculator/cs38/CalculatorGUI.java
+++ src/eg/edu/alexu/csd/oop/calculator/cs38/CalculatorGUI.java
@@ -1,169 +1,231 @@
 package eg.edu.alexu.csd.oop.calculator.cs38;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicOptionPaneUI.ButtonActionListener;
 
-public class CalculatorGUI {
+public class CalculatorGUI extends JPanel{
 	
-	final static boolean shouldFill = true;
-    final static boolean shouldWeightX = true;
-    final static boolean RIGHT_TO_LEFT = false;
-    
-    public static void addToPane(Container pane) {
-    	
-        if (RIGHT_TO_LEFT) {
-            pane.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
-        }
-        
-        
+	String input = "";
+	Calculator calculate = new SimpleCalculator();
+	JTextField screen = new JTextField();
+	JButton b0 = new JButton("0");
+	JButton b1 = new JButton("1");
+	JButton b2 = new JButton("2");
+	JButton b3 = new JButton("3");
+	JButton b4 = new JButton("4");
+	JButton b5 = new JButton("5");
+	JButton b6 = new JButton("6");
+	JButton b7 = new JButton("7");
+	JButton b8 = new JButton("8");
+	JButton b9 = new JButton("9");
+	JButton bDot = new JButton(".");
+	JButton bPlus = new JButton("+");
+	JButton bMinus = new JButton("-");
+	JButton bTimes = new JButton("*");
+	JButton bDivided = new JButton("/");
+	JButton bEqual = new JButton("=");
+	JButton bPrev = new JButton("Prev");
+	JButton bCur = new JButton("Current");
+	JButton bNex = new JButton("Next");
+	JButton bSave = new JButton("Save");
+	JButton bLoad = new JButton("Load");
+	JButton bClear = new JButton("Clear");
 
-        JButton button;
-        pane.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-        if (shouldFill) {
-            c.fill = GridBagConstraints.BOTH;
-        }
+	public CalculatorGUI() {
+		
+		JPanel panelC = new JPanel(new GridLayout(2,3));
 
-        JTextField screen= new JTextField();
-        if (shouldWeightX) {
-            c.weightx = 0.5;
-            c.weighty = 0.5;
-        }
-        c.gridx=0;
-        c.gridy=0;
-        c.gridwidth= 5;
-        pane.add(screen);
-        
-        button = new JButton("SAVE");
-        c.gridwidth=2;
-        c.gridx = 0;
-        c.gridy = 1;
-        pane.add(button, c);
-        
-        button = new JButton("LOAD");
-        c.gridx = 2;
-        c.gridy = 1;
-        pane.add(button, c);
-        
-        button = new JButton("PREV");
-        c.gridwidth=1;
-        c.gridx = 0;
-        c.gridy = 2;
-        pane.add(button, c);
+	    panelC.add(bSave);
+	    panelC.add(bLoad);
+	    panelC.add(bClear);
+	    panelC.add(bPrev);
+	    panelC.add(bCur);
+	    panelC.add(bNex);
+	    
+	    JPanel panel1 = new JPanel(new GridLayout(4,4));
+	    panel1.add(b7);
+	    panel1.add(b8);
+	    panel1.add(b9);
+	    panel1.add(bTimes);
+	    panel1.add(b4);
+	    panel1.add(b5);
+	    panel1.add(b6);
+	    panel1.add(bMinus);
+	    panel1.add(b1);
+	    panel1.add(b2);
+	    panel1.add(b3);
+	    panel1.add(bPlus);
+	    panel1.add(b0);
+	    panel1.add(bDot);
+	    panel1.add(bDivided);
+	    panel1.add(bEqual);
+	    
 
-        button = new JButton("CURRENT");
-        c.gridx = 1;
-        c.gridy = 2;
-        pane.add(button, c);
+	    ButtonListener listener = new ButtonListener();
+	    
+	    
+	    b0.setFont(b0.getFont().deriveFont(22.0f));
+	    b1.setFont(b0.getFont().deriveFont(22.0f));
+	    b2.setFont(b0.getFont().deriveFont(22.0f));
+	    b3.setFont(b0.getFont().deriveFont(22.0f));
+	    b4.setFont(b0.getFont().deriveFont(22.0f));
+	    b5.setFont(b0.getFont().deriveFont(22.0f));
+	    b6.setFont(b0.getFont().deriveFont(22.0f));
+	    b7.setFont(b0.getFont().deriveFont(22.0f));
+	    b8.setFont(b0.getFont().deriveFont(22.0f));
+	    b9.setFont(b0.getFont().deriveFont(22.0f));
+	    bDot.setFont(b0.getFont().deriveFont(22.0f));
+	    bPlus.setFont(b0.getFont().deriveFont(22.0f));
+	    bMinus.setFont(b0.getFont().deriveFont(22.0f));
+	    bTimes.setFont(b0.getFont().deriveFont(22.0f));
+	    bDivided.setFont(b0.getFont().deriveFont(22.0f));
+	    bEqual.setFont(b0.getFont().deriveFont(22.0f));
+	    
+	    b0.addActionListener(listener);
+	    b1.addActionListener(listener);
+	    b2.addActionListener(listener);
+	    b3.addActionListener(listener);
+	    b4.addActionListener(listener);
+	    b5.addActionListener(listener);
+	    b6.addActionListener(listener);
+	    b7.addActionListener(listener);
+	    b8.addActionListener(listener);
+	    b9.addActionListener(listener);
+	    bDot.addActionListener(listener);
+	    bPlus.addActionListener(listener);
+	    bMinus.addActionListener(listener);
+	    bTimes.addActionListener(listener);
+	    bDivided.addActionListener(listener);
+	    bPrev.addActionListener(listener);
+	    bCur.addActionListener(listener);
+	    bNex.addActionListener(listener);
+	    bSave.addActionListener(listener);
+	    bLoad.addActionListener(listener);
+	    bClear.addActionListener(listener);
+	    bEqual.addActionListener(listener);
+	    
+	    JPanel p1= new JPanel();
+	    p1.setLayout(new BorderLayout());
+	    p1.add(screen, BorderLayout.NORTH);
+	    p1.add(panelC, BorderLayout.CENTER);
 
-        button = new JButton("NEXT");
-        c.gridx = 2;
-        c.gridy = 2;
-        pane.add(button, c);
-        
-        button = new JButton("/");
-        c.gridx = 3;
-        c.gridy = 2;
-        pane.add(button, c);
-        
-        button = new JButton("7");
-        c.gridx = 0;
-        c.gridy = 3;
-        pane.add(button, c);
+	    setLayout(new BorderLayout());
+	    add(p1, BorderLayout.NORTH);
+	    add(panel1, BorderLayout.CENTER);
+	    screen.setHorizontalAlignment(SwingConstants.RIGHT);
+	    screen.setPreferredSize(new Dimension(300, 100));
+	    screen.setFont(screen.getFont().deriveFont(30.0f));
 
-        button = new JButton("8");
-        c.gridx = 1;
-        c.gridy = 3;
-        pane.add(button, c);
-        
-        button = new JButton("9");
-        c.gridx = 2;
-        c.gridy = 3;
-        pane.add(button, c);
-        
-        button = new JButton("*");
-        c.gridx = 3;
-        c.gridy = 3;
-        pane.add(button, c);
-        
-        button = new JButton("4");
-        c.gridx = 0;
-        c.gridy = 4;
-        pane.add(button, c);
+	}
 
-        button = new JButton("5");
-        c.gridx = 1;
-        c.gridy = 4;
-        pane.add(button, c);
-        
-        button = new JButton("6");
-        c.gridx = 2;
-        c.gridy = 4;
-        pane.add(button, c);
-        
-        button = new JButton("-");
-        c.gridx = 3;
-        c.gridy = 4;
-        pane.add(button, c);
-        
-        button = new JButton("1");
-        c.gridx = 0;
-        c.gridy = 5;
-        pane.add(button, c);
+	public static void main(String[] args) {
+	    SwingUtilities.invokeLater(new Runnable() {
 
-        button = new JButton("2");
-        c.gridx = 1;
-        c.gridy = 5;
-        pane.add(button, c);
-        
-        button = new JButton("3");
-        c.gridx = 2;
-        c.gridy = 5;
-        pane.add(button, c);
-        
-        button = new JButton("+");
-        c.gridx = 3;
-        c.gridy = 5;
-        pane.add(button, c);
+	        public void run() {
+	        	JFrame.setDefaultLookAndFeelDecorated(true);
+	            JFrame frame = new JFrame("Calculator");
+	            frame.add(new CalculatorGUI());
+	            frame.setSize(350, 500);
+	            frame.isResizable();
+	            frame.setVisible(true);
 
-        button = new JButton("0");
-        c.gridwidth = 2;
-        c.gridx = 0;
-        c.gridy = 6;
-        pane.add(button, c);
-        
-        button = new JButton(".");
-        c.gridwidth=1;
-        c.gridx = 2;
-        c.gridy = 6;
-        pane.add(button, c);
-        
-        button = new JButton("=");
-        c.gridheight=2;
-        c.gridx = 3;
-        c.gridy = 6;
-        pane.add(button, c);
+	        }
+	    });
+	}
 
-    }
-	
-  
-    private static void createGUI() {
-    	
-        JFrame.setDefaultLookAndFeelDecorated(true);
-        JFrame frame = new JFrame("Calculator");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        addToPane(frame.getContentPane());
-        frame.pack();
-        frame.setVisible(true);
-    }
+	class ButtonListener implements ActionListener {
 
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createGUI();
-            }
-        });
-    }
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+	    	if (e.getSource() == b0) {
+	            input += "0";
+	            screen.setText(input);
+	    	} else if (e.getSource() == b1) {
+	            input += "1";
+	            screen.setText(input);
+	        } else if (e.getSource() == b2) {
+	            input += "2";
+	            screen.setText(input);
+	        } else if (e.getSource() == b3) {
+	            input += "3";
+	            screen.setText(input);
+	        } else if (e.getSource() == b4) {
+	            input += "4";
+	            screen.setText(input);
+	        } else if (e.getSource() == b5) {
+	            input += "5";
+	            screen.setText(input);
+	        } else if (e.getSource() == b6) {
+	            input += "6";
+	            screen.setText(input);
+	        } else if (e.getSource() == b7) {
+	            input += "7";
+	            screen.setText(input);
+	        } else if (e.getSource() == b8) {
+	            input += "8";
+	            screen.setText(input);
+	        } else if (e.getSource() == b9) {
+	            input += "9";
+	            screen.setText(input);
+	        } else if (e.getSource() == bPlus) {
+	            input += "+";
+	            screen.setText(input);
+	        }else if (e.getSource() == bDot) {
+	        	int size=input.length();
+	        	if(size>=2) {
+	        		if(input.charAt(size-1)=='.') {
+			        	input="";
+			        	screen.setText("ERROR");
+			        }
+	        	}
+	        	if(size==0 || input.charAt(size-1)=='+'||input.charAt(size-1)=='-'||input.charAt(size-1)=='*'||input.charAt(size-1)=='/') {
+	        		input+="0";
+	       		}
+	       		input += ".";
+	       		screen.setText(input);
+	        } else if (e.getSource() == bMinus) {
+	            input += "-";
+	            screen.setText(input);
+	        } else if (e.getSource() == bTimes) {
+	            input += "*";
+	            screen.setText(input);
+	        }else if (e.getSource() == bDivided) {
+	            input += "/";
+	            screen.setText(input);
+	        } else if (e.getSource() == bClear) {
+	            input="";
+	            screen.setText(input);
+	        } else if (e.getSource() == bPrev) {
+	        	input="";
+	            screen.setText(calculate.prev());
+	        }
+	         else if (e.getSource() == bCur) {
+		         screen.setText(calculate.current());
+	        }
+	         else if (e.getSource() == bNex) {
+		         input="";
+		         screen.setText(calculate.next());
+	        }else if (e.getSource() == bSave) {
+	        	calculate.save();
+		        input="";
+		        screen.setText("SAVED");
+	        }
+	         else if (e.getSource() == bLoad) {
+	        	 calculate.load();
+			     input="";
+			     screen.setText("LOADED");
+	        }
+	         else if (e.getSource() == bEqual) {
+	            calculate.input(input);
+	            input="";
+	            screen.setText(calculate.getResult());
+	        }
+	    }
+
+	}
     
 }
--- src/eg/edu/alexu/csd/oop/calculator/cs38/SimpleCalculator.java
+++ src/eg/edu/alexu/csd/oop/calculator/cs38/SimpleCalculator.java
@@ -12,6 +12,7 @@ public class SimpleCalculator implements Calculator {
 	public static int error = 0;
 	public static int N=0;
 	public static String result=null;
+	public static int PREV,NEX = 0;
 	
 	public void arrangeMemory() {
 		now=0;
@@ -76,7 +77,10 @@ public class SimpleCalculator implements Calculator {
 				if(mS.find()) {
 					if(N==1) {
 						temp[0][i] = (mS.group());
-						s=s.substring(0, mS.start())+s.substring(mS.start()+1);
+						s=s.substring(mS.start()+1);
+						if(temp[0][1].equals("-")) {
+							m1=num1.matcher(s);
+						}
 						i++;
 						error=0;
 					}
@@ -86,7 +90,6 @@ public class SimpleCalculator implements Calculator {
 					}
 				}
 			}
-		}
 			if(error==0) {
 				arrangeMemory();
 				current[0][0]=String.valueOf(temp[0][0]);
@@ -95,16 +98,24 @@ public class SimpleCalculator implements Calculator {
 				current[0][3]=String.valueOf(temp[0][3]);
 			}
 		}
+	}
 
 	@Override
 	public String getResult() {
+		if(error==1 || N < 2) {
+			return "ERROR";
+		}
 		double n1=Double.valueOf(current[0][0]);
 		double n2=Double.valueOf(current[0][2]);
 		if(n1%1==0) {
-			current[0][0]=String.valueOf((int)n1);
+			Matcher m = Pattern.compile("-\\d+|\\d+").matcher(String.valueOf(current[0][0]));
+			m.find();
+			current[0][0]=String.valueOf(m.group());
 		}
 		if(n2%1==0) {
-			current[0][2]=String.valueOf((int)n2);
+			Matcher m = Pattern.compile("-\\d+|\\d+").matcher(String.valueOf(current[0][2]));
+			m.find();
+			current[0][2]=String.valueOf(m.group());
 		}
 		String r="";
 		if(current[0][1].equals("+")) {
@@ -126,22 +137,34 @@ public class SimpleCalculator implements Calculator {
 			}
 		}
 		if(Double.valueOf(current[0][3])%1 == 0) {
-			Matcher m = Pattern.compile("\\d+").matcher(current[0][3]);
+			Matcher m = Pattern.compile("\\d+|-\\d+").matcher(current[0][3]);
 			m.find();
 			current[0][3]=m.group();
 		}
-		r=current[0][0] + current[0][1] + current[0][2] + "=" + current[0][3];
+		r=current[0][3];
 		return r;
 	}
 
 	@Override
 	public String current() {
+		if(String.valueOf(current[0][3]).equals("null")) {
+			return "ERROR: No Current";
+		}
 		return current[0][0] + current[0][1] + current[0][2] + "=" + current[0][3] ;
 	}
 
 	@Override
 	public String prev() {
-		if(now==4) {
+		PREV=1;
+		if(NEX==1) {
+			NEX=0;
+			now++;
+			now++;
+		}
+		if(now==-1) {
+			now++;
+		}
+		if(now==5 || String.valueOf(memory[now][3]).equals("null")) {
 			return "ERROR: No Prev";
 		}
 		else {
@@ -156,7 +179,16 @@ public class SimpleCalculator implements Calculator {
 
 	@Override
 	public String next() {
-		if(now==0) {
+		NEX=1;
+		if(PREV==1) {
+			PREV=0;
+			now--;
+			now--;
+		}
+		if(now==5) {
+			now--;
+		}
+		if(now==-1 || String.valueOf(memory[now][3]).equals("null")) {
 			return "ERROR: No Next";
 		}
 		else {
@@ -191,39 +223,47 @@ public class SimpleCalculator implements Calculator {
 	@Override
 	public void load() {
 		Scanner loadInfo = null;
+		int iLoad = 0;
 		try {
 			loadInfo= new Scanner(new File("SaveInfo.txt"));
 		}catch(Exception e){
 			error=1;
 		}
 		String temp = loadInfo.nextLine();
-		String number1 ="\\d+\\.\\d+|\\d+";
+		String number1 ="\\d+\\.\\d+|\\d+|-\\d+\\.\\d+|-\\d+";
 		String symbols = "[+/*-]";
 		Pattern num1 = Pattern.compile(number1);
 		Pattern sym = Pattern.compile(symbols);
 		Matcher m1 = num1.matcher(temp);
-		Matcher mS = sym.matcher(temp);
-		int i = 0;
+		Matcher mS = sym.matcher(temp); 
 		while (m1.find()) {
-			current[0][i]= m1.group();
-			i++;
-			if(mS.find()) {
-				current[0][i] = (mS.group());
-				i++;
+			current[0][iLoad]= m1.group();
+			iLoad++;
+			if(mS.find() && iLoad<3) {
+				current[0][iLoad] = (mS.group());
+				iLoad++;
 			}
 		}
-		for(int j=0; j<5;j++) {
+		for(int jLoad=0; jLoad<5;jLoad++) {
 			temp=loadInfo.nextLine();
 			m1 = num1.matcher(temp);
 			mS = sym.matcher(temp);
 			int k=0;
 			while (m1.find()) {
-				memory[j][k]= m1.group();
+				memory[jLoad][k]= m1.group();
 				k++;
-				if(mS.find()) {
-					memory[j][k] = (mS.group());
+				if(mS.find()&& k<3) {
+					memory[jLoad][k] = (mS.group());
 					k++;
 				}
+			}
+		}
+		if(String.valueOf(current[0][3]).equals("null")) {
+			current[0][3]="MATH ERROR";
+		}
+		for(int er=0;er<5;er++) {
+			if(String.valueOf(memory[er][3]).equals("null")) {
+				memory[er][3]="MATH ERROR";
 			}
 		}
 		loadInfo.close();
@@ -241,17 +281,8 @@ public class SimpleCalculator implements Calculator {
 			IP=sc.nextLine();
 			SimpleCalculator calc = new SimpleCalculator();
 			calc.input(IP);
-			if(error==1 || N < 2) {
-				System.out.println("ERROR");
-				continue;
-			}
-			if(IP.equals("save")||IP.equals("SAVE")||IP.equals("load")||IP.equals("LOAD")) {
-				System.out.println(result);
-			}else {
-				result = calc.getResult();
-				System.out.println(result);
-			}
-			System.out.println("");
+			result = calc.getResult();
+			System.out.println(result);
 		}
 	}
 
